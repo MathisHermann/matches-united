47b25f8f88cc81a5d138020286f9ee9c5dded4dc
Mathis Hermann <mathis.hermann@students.fhnw.ch>
2023-02-08 15:57:06 +0100

    Add Ranking

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,27 @@
 <script setup>
 import List from './components/List.vue'
+import Ranking from './components/Ranking.vue'
 </script>
 
 <script>
 
 const UNIHOCKEY_API_URL = `https://api-v2.swissunihockey.ch/api/games?mode=team&season=2022&team_id=428375`
+const TABLE_API_URL = `https://api-v2.swissunihockey.ch/api/rankings?league=5&season=2022&game_class=12&group=Gruppe%204`
 
 export default {
   data() {
     return {
       headers: ['header'],
       dates: null,
+      ranking: null,
       page: null,
       pages: [1, 2],
+      current_tab: "Matches"
     }
   },
   created() {
     // fetch on init
+    this.fetchRankings()
     this.fetchUnihockeyData()
   },
   methods: {
@@ -26,6 +31,13 @@ export default {
       this.dates = response.data.regions[0].rows
       this.headers = response.data.headers
       this.page = response.data.context.page
+    },
+    async fetchRankings() {
+      let response = await (await fetch(TABLE_API_URL)).json()
+      this.ranking = response.data.regions
+    },
+    selectTab(tab) {
+      this.current_tab = tab
     }
   },
   watch: {
@@ -54,17 +66,28 @@ export default {
       </div>
     </nav>
 
-
     <div class="mx-auto max-w-7xl py-10">
       <header>
         <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
           <div class="flex flex-row justify-between">
-            <h1 class="text-3xl font-bold leading-tight tracking-tight text-gray-900">Matches</h1>
-
-            <div class="flex flex-row space-x-4 items-center">
+            <div>
+              <a v-on:click="selectTab('Matches')" type="button"
+                class="cursor-pointer inline-flex items-center rounded-l bg-white py-2 text-base font-medium text-gray-700 hover:bg-red-50">
+                <h1 class="text-xl sm:text-3xl border-r border-gray-400 px-4 leading-tight tracking-tight text-gray-900" :class="{'font-bold': current_tab == 'Matches'}">
+                  Matches
+                </h1>
+              </a>
+              <a v-on:click="selectTab('Ranking')" type="button"
+                class="cursor-pointer inline-flex items-center rounded-r bg-white px-4 py-2 text-base font-medium text-gray-700 hover:bg-red-50">
+                <h1 class="text-xl sm:text-3xl leading-tight tracking-tight text-gray-900" :class="{'font-bold': current_tab == 'Ranking'}">
+                  Ranking
+                </h1>
+              </a>
+            </div>
+            <div v-if="current_tab == 'Matches'" class="flex flex-row space-x-4 items-center">
               <label for="page" class="block text-sm font-medium text-gray-700">Seite</label>
               <select v-model="page" id="page" name="page"
-                class="mt-1 block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm">
+                class="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-red-500 focus:outline-none focus:ring-red-500 sm:text-sm">
                 <option disabled value="">-</option>
                 <template v-for="page in pages">
                   <option :id="page" :value="page">
@@ -74,11 +97,11 @@ export default {
               </select>
             </div>
           </div>
-
+          
         </div>
       </header>
-
-      <List :dates="dates" :headers="headers" />
+      <List v-if="current_tab == 'Matches'" :dates="dates" :headers="headers" />
+      <Ranking v-if="current_tab == 'Ranking'" :ranking="ranking" />
     </div>
   </div>
 </template>
---
+++ src/components/Ranking.vue
@@ -0,0 +1,92 @@
+<script setup>
+
+defineProps({
+    ranking: Array,
+})
+
+</script>
+
+<template>
+    <div class="px-4 sm:px-6 lg:px-8">
+        <div class="-mx-4 mt-8 overflow-hidden shadow ring-1 ring-black ring-opacity-5 sm:-mx-6 md:mx-0 md:rounded-lg">
+            <table class="min-w-full divide-y divide-gray-300">
+                <thead class="bg-gray-50">
+                    <tr> 
+                        <th scope="col"
+                            class="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-6 lg:table-cell">
+                            Rang</th>
+                        <th scope="col"
+                            class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Team
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">Sp
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">SoW
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">S
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">U
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">N
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 sm:table-cell">T
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">TD
+                        </th>
+                        <th scope="col"
+                            class="hidden px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">PQ
+                        </th>
+                        <th scope="col" class="px-3 py-3.5 text-left text-sm font-semibold text-gray-900 lg:table-cell">
+                            P
+                        </th>
+
+                    </tr>
+                </thead>
+                <tbody class="divide-y divide-gray-200 bg-white">
+                    <tr v-for=" item in ranking[0].rows">
+                        <td
+                            class=" max-w-0 py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:w-auto sm:max-w-none sm:pl-6">
+                            {{ item.cells[0].text[0] }}
+                        </td>
+                        <td class="whitespace-nowrap py-4 pl-4 pr-3 text-sm sm:pl-6 lg:table-cell">
+                            <div class="flex flex-col lg:flex-row items-left lg:items-center">
+                                <div class="h-10 w-10 flex-shrink-0 mr-4">
+                                    <img class="h-10 w-10 rounded-full" :src="item.cells[1].image.url"
+                                        alt="Logo">
+                                </div>
+                                <div class="">
+                                    <div class="font-medium text-gray-900">{{ item.cells[2].text[0] }}</div>
+                                </div>
+                            </div>
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ item.cells[3].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ item.cells[4].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ item.cells[5].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ item.cells[6].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ item.cells[7].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 sm:table-cell">{{ item.cells[8].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ item.cells[9].text[0] }}
+                        </td>
+                        <td class="hidden px-3 py-4 text-sm text-gray-500 lg:table-cell">{{ item.cells[10].text[0] }}
+                        </td>
+                        <td class="px-3 py-4 text-sm text-gray-500 table-cell font-bold">{{ item.cells[11].text[0] }}</td>
+                    </tr>
+
+                </tbody>
+            </table>
+        </div>
+    </div>
+</template>
+
